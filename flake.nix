bad4d9e9c28be208c7ffb8f999830ac4c7d52ad3
Raghav Sood <r@raghavsood.com>
2024-09-02 17:40:49 +0800

    flake: remove debug symbols

--- flake.nix
+++ flake.nix
@@ -19,36 +19,11 @@
 
           buildInputs = with pkgs; [ openssl pcre ];
 
-          cmakeBuildType = "Debug";
-          cmakeFlags = [
-            "-DCMAKE_BUILD_TYPE=Debug"
-
-          ];
-
-          dontStrip = true;
-
           installPhase = ''
             mkdir -p $out/bin
             cp vanitygen $out/bin
             cp keyconv $out/bin/vanitygen-keyconv
           '';
-
-          meta = with pkgs.lib; {
-            description = "Bitcoin vanity address generator";
-            longDescription = ''
-              Vanitygen can search for exact prefixes or regular expression
-              matches, so you can generate Bitcoin addresses that starts
-              with the needed mnemonic.
-              Vanitygen can generate regular bitcoin addresses, namecoin
-              addresses, and testnet addresses.
-              When searching for exact prefixes, vanitygen will ensure that
-              the prefix is possible, will provide a difficulty estimate,
-              and will run about 30% faster.
-            '';
-            homepage = "https://github.com/samr7/vanitygen";
-            # license = licenses.agpl3;
-            platforms = platforms.all;
-          };
         };
       }
     );
